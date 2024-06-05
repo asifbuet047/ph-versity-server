df18841f0a6e20876540d3acdec96004f8384596
asifbuet047 <asifbuet047@gmail.com>
2024-06-06 01:14:52 +0600

    express middlewire with custom parameter in route level

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -3,16 +3,17 @@ import { UserController } from "./user.controller";
 
 const UserRoute = express.Router();
 
-const routeLevelMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  console.log("I am a amiddle wire at route level");
-  console.log(req);
-  next();
+const routeLevelMiddleware = (param: string) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    console.log(param);
+    next();
+  };
 };
 
-UserRoute.post("/add-student", routeLevelMiddleware, UserController.addStudent);
+UserRoute.post(
+  "/add-student",
+  routeLevelMiddleware("Test parametered midldewire"),
+  UserController.addStudent,
+);
 
 export default UserRoute;
